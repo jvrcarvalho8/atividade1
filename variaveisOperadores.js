fca0d4f0216a90f389685033fc79627cc04eef72
João Vítor Rocha de Carvalho <110432139+jvrcarvalho8@users.noreply.github.com>
2024-03-14 23:32:16 -0300

    variaveisOperadores.js

--- variaveisOperadores.js
+++ variaveisOperadores.js
@@ -1,17 +1,14 @@
-// Declaração das variáveis numéricas
-let numA1 = 10;
-let numB1 = 5;
-let numA2 = 20;
-let numB2 = 3;
+let nmrA1 = 22;
+let nmrB1 = 6;
+let nmrA2 = 34;
+let nmrB2 = 2;
 
-// Operações matemáticas
-let resultadoSoma = numA1 + numB1;
-let resultadoSubtracao = numA1 - numB1;
-let resultadoMultiplicacao = numA2 * numB2;
-let resultadoDivisao = numA2 / numB2;
+let resultadoSoma = nmrA1 + nmrB1;
+let resultadoSub = nmrA1 - nmrB1;
+let resultadoMulti = nmrA2 * nmrB2;
+let resultadoDiv = nmrA2 / nmrB2;
 
-// Impressão dos resultados
-console.log('Resultado da soma entre ${numA1} e ${numB1}: ${resultadoSoma}');
-console.log('Resultado da subtração entre ${numA1} e ${numB1}: ${resultadoSubtracao}');
-console.log('Resultado da multiplicação entre ${numA2} e ${numB2}: ${resultadoMultiplicacao}');
-console.log('Resultado da divisão entre ${numA2} e ${numB2}: ${resultadoDivisao}');
+console.log("Resultado da soma entre ",nmrA1," e ",nmrB1,":",resultadoSoma);
+console.log("Resultado da subtração entre ",nmrA1," e ",nmrB1,":",resultadoSub);
+console.log("Resultado da multiplicação entre ",nmrA2," e ",nmrB2,":",resultadoMulti);
+console.log("Resultado da divisão entre ",nmrA2," e ",nmrB2,":",resultadoDiv);
