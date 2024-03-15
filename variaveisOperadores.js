38fed5ab981f8feb700947e1a0f92e88d91d0344
João Vítor Rocha de Carvalho <110432139+jvrcarvalho8@users.noreply.github.com>
2024-03-14 23:05:08 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,17 @@
+// Declaração das variáveis numéricas
+let numA1 = 10;
+let numB1 = 5;
+let numA2 = 20;
+let numB2 = 3;
+
+// Operações matemáticas
+let resultadoSoma = numA1 + numB1;
+let resultadoSubtracao = numA1 - numB1;
+let resultadoMultiplicacao = numA2 * numB2;
+let resultadoDivisao = numA2 / numB2;
+
+// Impressão dos resultados
+console.log('Resultado da soma entre ${numA1} e ${numB1}: ${resultadoSoma}');
+console.log('Resultado da subtração entre ${numA1} e ${numB1}: ${resultadoSubtracao}');
+console.log('Resultado da multiplicação entre ${numA2} e ${numB2}: ${resultadoMultiplicacao}');
+console.log('Resultado da divisão entre ${numA2} e ${numB2}: ${resultadoDivisao}');
